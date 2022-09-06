8ef990e49136ccaec5d9f39b9aacd749c2ad9405
waynerbarrios <wbarrios@uninorte.edu.co>
2022-09-05 21:48:53 -0500

    Otra nueva version

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,7 +4,6 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
-        miVertebrados[3] = new Vertebrado("Perro", "Mamifero"); 
         
         return miVertebrados;
     }
